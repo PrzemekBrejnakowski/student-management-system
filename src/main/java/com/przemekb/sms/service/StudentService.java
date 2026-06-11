e409e97a298dd1299e77e9495b9e0d32d587619d
User <przemek.brejnakowski@gmail.com>
2026-06-11 16:32:00 +0200

    add Student

---
+++ src/main/java/com/przemekb/sms/service/StudentService.java
@@ -0,0 +1,11 @@
+package com.przemekb.sms.service;
+
+import com.przemekb.sms.entity.Student;
+
+import java.util.List;
+
+public interface StudentService {
+
+    List<Student> getAllStudents();
+
+}
---
+++ src/main/java/com/przemekb/sms/service/impl/StudentServiceImpl.java
@@ -0,0 +1,23 @@
+package com.przemekb.sms.service.impl;
+
+import com.przemekb.sms.entity.Student;
+import com.przemekb.sms.repository.StudentRepository;
+import com.przemekb.sms.service.StudentService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+}
